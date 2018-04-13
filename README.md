b7732c2c23d78f9f42e06083ab178a8bf6d7f78d
Nikolay Dyakov <dyakov.nikolay@gmail.com>
2018-04-13 11:49:48 +0300

    Init

---
+++ README.md
@@ -0,0 +1,13 @@
+# Small script to generate bcrypt hash based on password and cost
+
+* `go build -o bcrypt main.go`
+
+```bash
+Usage of bcrypt:
+  -c int
+    	Cost (default 10)
+  -cc
+    	Copy uuid to clipboard
+  -p string
+    	Password to hash (default "bcrypt")
+```
---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"flag"
+	"golang.org/x/crypto/bcrypt"
+	"github.com/atotto/clipboard"
+)
+
+func main() {
+	password := flag.String("p", "bcrypt", "Password to hash")
+	cost := flag.Int("c", 10, "Cost")
+	copy := flag.Bool("cc", false, "Copy uuid to clipboard")
+	flag.Parse()
+	hash, err := bcrypt.GenerateFromPassword([]byte(*password), *cost)
+	if err != nil {
+		fmt.Println("Sorry cant generate hash for that password")
+	} else if *copy {
+		clipboard.WriteAll(string(hash))
+	} else {
+		fmt.Println(string(hash))
+	}
+}
